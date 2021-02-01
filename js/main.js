daa503bf356aa03136f8dd6e0de1f90573791855
RomanTkachev <not.so.bad@yandex.ru>
2021-02-01 12:46:22 +0300

    Добавляет проверку на max < min в функции, меняет названия функций и аргументов

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
-const getRandomInteger = (min, max) => {
+const getRandomBetween = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min >= 0 && max >= 0 && min < max) {
-    return Math.floor(Math.random() * (max - min + 1) + min); // Подсмотрел логику на MDN
-  } else if (min < 0 || max < 0) {
+  if (min < 0 || max < 0) {
     throw new Error('Ошибка: введено отлицательное значение');
-  } else {
-    throw new Error('Ошибка: Минимальное значение больше или равно максимальному');
+  }
+
+  if (max < min) {
+    let swap = 0;
+    swap = max;
+    max = min;
+    min =swap;
+  }
+
+  if (min >= 0 && max >= 0 && min < max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 };
 
 getRandomInteger(10, 150);
 
-const getRandomFloat = (min, max, symbolsNumber) => {
-  if (min >= 0 && max >= 0 && min < max) {
-    return Number((Math.random() * (max - min + 1) + min).toFixed(symbolsNumber));
-  } else if (min < 0 || max < 0) {
+const getRandomFloat = (min, max, symbolsDigit) => {
+
+  if (min < 0 || max < 0) {
     throw new Error('Ошибка: введено отлицательное значение');
-  } else {
-    throw new Error('Ошибка: Минимальное значение больше или равно максимальному');
+  }
+
+  if (max < min) {
+    let swap = 0;
+    swap = max;
+    max = min;
+    min =swap;
+  }
+
+  if (min >= 0 && max >= 0 && min < max) {
+    return Number((Math.random() * (max - min) + min).toFixed(symbolsNumber));
   }
 };
 
