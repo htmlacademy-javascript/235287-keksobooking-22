0400adfd586f9de3b7a3f8a72cabf0fd147e17ab
RomanTkachev <not.so.bad@yandex.ru>
2021-03-06 00:07:39 +0300

    добавляет валидацию формы

--- js/form.js
+++ js/form.js
@@ -1,3 +1,7 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE_VALUE = 1000000;
+
 const MIN_PRICES = {
   bungalow: 0,
   flat: 1000,
@@ -15,6 +19,10 @@ const formInputPrice = adForm.querySelector('#price');
 const formInputCheckIn = adForm.querySelector('#timein');
 const formInputCheckOut = adForm.querySelector('#timeout');
 const formInputAdress = adForm.querySelector('#address');
+const formInputTitle = adForm.querySelector('#title');
+const formInputRoomNumber = adForm.querySelector('#room_number');
+const formInputCapacity = adForm.querySelector('#capacity');
+
 
 const deactivateForm = () => {
   formInteractiveElements.forEach((formElement) => {
@@ -66,6 +74,71 @@ const setMinPrices = () => {
     formInputPrice.min = MIN_PRICES[formInputType.value];
   });
 }
+
+formInputTitle.addEventListener('input', () => {
+  const valueLength = formInputTitle.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    formInputTitle.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    formInputTitle.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) +' симв.');
+  } else {
+    formInputTitle.setCustomValidity('');
+  };
+
+  formInputTitle.reportValidity();
+});
+
+formInputPrice.addEventListener('input', () => {
+  const inputValue = formInputPrice.value;
+
+  if (inputValue > MAX_PRICE_VALUE) {
+    formInputPrice.setCustomValidity('Максимальная цена за ночь: ' + MAX_PRICE_VALUE)
+  } else {
+    formInputPrice.setCustomValidity('');
+  };
+
+  formInputTitle.reportValidity();
+});
+
+formInputRoomNumber.addEventListener('change', (evt) => {
+  switch (evt.target.value) {
+    case '1':
+      formInputCapacity.options[0].disabled = true;
+      formInputCapacity.options[1].disabled = true;
+      formInputCapacity.options[2].disabled = false;
+      formInputCapacity.options[3].disabled = true;
+      formInputCapacity.options[2].selected = true;
+      break;
+    case '2':
+      formInputCapacity.options[0].disabled = true;
+      formInputCapacity.options[1].disabled = false;
+      formInputCapacity.options[2].disabled = false;
+      formInputCapacity.options[3].disabled = true;
+      formInputCapacity.options[2].selected = true;
+      break;
+    case '3':
+      formInputCapacity.options[0].disabled = false;
+      formInputCapacity.options[1].disabled = false;
+      formInputCapacity.options[2].disabled = false;
+      formInputCapacity.options[3].disabled = true;
+      formInputCapacity.options[2].selected = true;
+      break;
+    case '100':
+      formInputCapacity.options[0].disabled = true;
+      formInputCapacity.options[1].disabled = true;
+      formInputCapacity.options[2].disabled = true;
+      formInputCapacity.options[3].disabled = false;
+      formInputCapacity.options[3].selected = true;
+      break;
+    default:
+      formInputCapacity.options[0].disabled = false;
+      formInputCapacity.options[1].disabled = false;
+      formInputCapacity.options[2].disabled = false;
+      formInputCapacity.options[3].disabled = false;
+      formInputCapacity.options[2].selected = true;
+  };
+});
 
 export {
   equalizeCheckInTime,
